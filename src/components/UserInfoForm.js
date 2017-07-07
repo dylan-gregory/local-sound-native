d7b5cc5201bede0e5541cad8f79fcd38ece76a1e
dylan-gregory <jdylangregory@gmail.com>
2017-07-07 10:34:03 -0400

    Now able to access camera roll

--- src/components/UserInfoForm.js
+++ src/components/UserInfoForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { connect } from 'react-redux';
+import ImagePicker from 'react-native-image-picker';
 import { userProfileCreate, userProfileUpdate, userProfileFetch } from '../actions';
 import { Container, Content, Header, Footer, FooterTab, Text, Card, CardItem, Left, Body, Icon, Thumbnail, Right, Form, Item, Input, Label } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -16,7 +17,8 @@ class UserInfoForm extends Component {
       bio: this.props.bio
     };
 
-    console.log(this.state);
+    console.log('state', this.state);
+    console.log('picker', ImagePicker);
 
   }
   // componentWillMount() {
@@ -26,6 +28,43 @@ class UserInfoForm extends Component {
   //   console.log('NextProps', nextProps);
   //
   // }
+  uploadPic(){
+
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [
+        {name: 'fb', title: 'Choose Photo from Facebook'},
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      }
+      else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      }
+      else {
+        let source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source
+        });
+      }
+    });
+  }
   onButtonPress() {
     // const { name, phone, bio } = this.props;
     // this.props.userProfileUpdate({ prop: 'name', value: this.state.name });
@@ -65,6 +104,10 @@ class UserInfoForm extends Component {
                     onChangeText={(text) => this.setState({ bio: text })}
                   />
                 </Item>
+
+                <Button onPress={this.uploadPic.bind(this)}>
+                  <Text>Upload photo?</Text>
+                </Button>
 
                 <Button onPress={this.onButtonPress.bind(this)} >
                   <Text>Update</Text>
